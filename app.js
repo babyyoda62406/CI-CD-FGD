889457e4a8e35615a3beb17e0a3af95cdfd2f13a
Deivis <deivis@dtm.debiab.com>
2024-05-09 14:42:16 -0400

    init: Initial commit to CI/CD system

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+
+const app = express();
+const puerto = 4000;
+
+app.use(bodyParser.json());
+
+
+
+app.post('/teodoro', (req, res) => {
+
+    console.log("Push Detectado")
+    const { ref } = req.body
+    switch (ref) {
+        case "refs/heads/v1":
+            CICD_teodoro((error, stdout, stderr) => {
+                if (error) {
+                    console.error('Error al ejecutar el comando de despliegue:', error);
+                } else {
+                    console.log('Despliegue exitoso:', stdout);
+                }
+            });
+
+            break;
+        default:
+            console.log(ref)
+    }
+
+    res.end()
+    
+});
+
+const CICD_teodoro = (callback) => {
+    exec('cd /root/totalrepairs   && git stash && git pull --hard && pnpm install && pm2 restart teodoro', callback);
+}
+
+
+app.listen(puerto, () => {
+    console.log(`CICD => ${puerto}`);
+});
+
+
+
+
