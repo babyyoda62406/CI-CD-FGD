c1cfbebaf2a64ec93cf76a60852446589a4a7e75
Deivis <deivis@dtm.debiab.com>
2024-05-09 14:43:57 -0400

    fix: Add build

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.post('/teodoro', (req, res) => {
 });
 
 const CICD_teodoro = (callback) => {
-    exec('cd /root/totalrepairs   && git stash && git pull --hard && pnpm install && pm2 restart teodoro', callback);
+    exec('cd /root/totalrepairs   && git stash && git pull --hard && pnpm install && npm run build && pm2 restart teodoro', callback);
 }
 
 
