15a394d4409ef0af1774611a2c8376aa0cf0f83a
babyyoda62406 <babyyoda62406@gmail.com>
2024-08-01 21:37:26 -0400

    cicd

--- app.js
+++ app.js
@@ -9,88 +9,33 @@ app.use(bodyParser.json());
 
 
 // Teodoro
-app.post('/teodoro', (req, res) => {
+app.post('/nextai', (req, res) => {
 
     console.log("Push Detectado")
     const { ref } = req.body
     switch (ref) {
-        case "refs/heads/v1":
-            CICD_teodoro((error, stdout, stderr) => {
+        case "refs/heads/main":
+            CICD_nextai((error, stdout, stderr) => {
                 if (error) {
                     console.error('Error al ejecutar el comando de despliegue:', error);
                 } else {
                     console.log('Despliegue exitoso:', stdout);
                 }
             });
-
             break;
         default:
             console.log(ref)
     }
 
     res.end()
-    
+
 });
 
-// Grafiska_Plastic
-app.post('/grafiska_plastic', (req, res) => {
 
-    console.log("Push Detectado")
-    const { ref } = req.body
-    switch (ref) {
-        case "refs/heads/v1":
-            CICD_Grafiska_Plastic((error, stdout, stderr) => {
-                if (error) {
-                    console.error('Error al ejecutar el comando de despliegue:', error);
-                } else {
-                    console.log('Despliegue exitoso:', stdout);
-                }
-            });
-
-            break;
-        default:
-            console.log(ref)
-    }
-
-    res.end()
-    
-});
-
-// Grafiska_Flower
-app.post('/grafiska_flower', (req, res) => {
-
-    console.log("Push Detectado")
-    const { ref } = req.body
-    switch (ref) {
-        case "refs/heads/v1":
-            CICD_Grafiska_Flower((error, stdout, stderr) => {
-                if (error) {
-                    console.error('Error al ejecutar el comando de despliegue:', error);
-                } else {
-                    console.log('Despliegue exitoso:', stdout);
-                }
-            });
-
-            break;
-        default:
-            console.log(ref)
-    }
-
-    res.end()
-    
-});
-
-const CICD_teodoro = (callback) => {
-    exec('cd /root/totalrepairs  && git stash && git pull && pnpm install && npm run build &&  pm2 restart teodoro', callback);
+const CICD_nextai = (callback) => {
+    exec('cd  // && cd jtb   && git stash && git pull && pnpm install && npm run build &&  pm2 restart njsruntime', callback);
 }
 
-const CICD_Grafiska_Plastic = (callback) => {
-    exec('cd /root/Grafiska_IMGPlastic/front  && git stash && git pull && pnpm install && npm run build', callback);
-}
-
-const CICD_Grafiska_Flower = (callback) => {
-    exec('cd /root/Grafiska_Flower/front  && git stash && git pull && pnpm install && npm run build', callback);
-}
 
 app.listen(puerto, () => {
     console.log(`CICD => ${puerto}`);
