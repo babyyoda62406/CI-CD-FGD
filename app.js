759cd4328474ee2496245e3db4ca5f2807b0c5ee
Deivis Torres Mena <babyyoda62406@gmail.com>
2024-08-26 15:31:53 -0400

    feat: CI-CD: Try 1

--- app.js
+++ app.js
@@ -7,59 +7,33 @@ const puerto = 4010;
 
 app.use(bodyParser.json());
 
-
-// nextai deply
-app.post('/nextai', (req, res) => {
-
-    console.log("Push Detectado")
-    const { ref } = req.body
+// Hotel Deploy
+app.post('/hotel', (req, res) => {
+    console.log("Push Detectado");
+    const { ref } = req.body;
     switch (ref) {
         case "refs/heads/main":
-            CICD_nextai((error, stdout, stderr) => {
+            CICD_hotel((error, stdout, stderr) => {
                 if (error) {
                     console.error('Error al ejecutar el comando de despliegue:', error);
+                    return res.status(500).send('Error en el despliegue');
                 } else {
                     console.log('Despliegue exitoso:', stdout);
+                    return res.status(200).send('Despliegue exitoso');
                 }
             });
             break;
         default:
-            console.log(ref)
+            console.log(ref);
+            return res.status(200).send('Referencia no manejada');
     }
-
-    res.end()
-
 });
 
-app.post('/nextai_reset', (req, res) => {
-
-    console.log("Push Detectado reset")
-    CICD_nextai_reset((error, stdout, stderr) => {
-        if (error) {
-            console.error('Error al ejecutar el comando de reset:', error);
-        } else {
-            console.log('reset exitoso:', stdout);
-        }
-    });
-
-    res.end()
-
-});
-
-
-const CICD_nextai = (callback) => {
-    exec('cd  // && cd jtb   && git stash && git pull && pnpm install && npm run build &&  pm2 restart njsruntime', callback);
+// Función de despliegue
+const CICD_hotel = (callback) => {
+    exec('rm -rf HotelDeutschesHaus && git clone git@github.com:FGDGines/HotelDeutschesHaus.git && cd HotelDeutschesHaus && pnpm install && npm run build && npm run up', callback);
 }
-
-const CICD_nextai_reset = (callback) => {
-    exec('pm2 restart njsruntime', callback);
-}
-
 
 app.listen(puerto, () => {
     console.log(`CICD => ${puerto}`);
 });
-
-
-
-
